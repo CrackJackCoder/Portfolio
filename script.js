54da3c03e7616d6518a92ea481b60a5fa9dd74b4
Harsk Kumar Singh <harshkumar23x@gmail.com>
2024-09-24 19:30:18 +0530

    link updated

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
 //Moving cage
-const cage=document.querySelector('.cages');
-const container=document.querySelector('.first-page');
+const cage = document.querySelector('.cages');
+const container = document.querySelector('.first-page');
 
-container.addEventListener("mousemove", function(e){
-    let Xaxis=(e.pageX);
-    let Yaxis=(e.pageY);
-    cage.style.transform=`translateY(${-Yaxis/20}px) translateX(${-Xaxis/20}px)`;
+container.addEventListener("mousemove", function (e) {
+    let Xaxis = (e.pageX);
+    let Yaxis = (e.pageY);
+    cage.style.transform = `translateY(${-Yaxis / 20}px) translateX(${-Xaxis / 20}px)`;
 });
 
 
 //drop-down
 
-const dropper=document.querySelector('.dropper');
-const menu=document.querySelector('.option');
-const arrow=document.querySelector('.arrow');
-dropper.addEventListener('click',function(e){
+const dropper = document.querySelector('.dropper');
+const menu = document.querySelector('.option');
+const arrow = document.querySelector('.arrow');
+dropper.addEventListener('click', function (e) {
     menu.classList.toggle('visible');
     arrow.classList.toggle('rotate');
 });
 
-const option=document.querySelector('.option');
-const selected=document.querySelector('.selected');
-const card=document.querySelectorAll('.card');
-option.addEventListener('click',function(e){
-    if(e.target.nodeName=='LI'){
-        selected.textContent=e.target.textContent;
+const option = document.querySelector('.option');
+const selected = document.querySelector('.selected');
+const card = document.querySelectorAll('.card');
+option.addEventListener('click', function (e) {
+    if (e.target.nodeName == 'LI') {
+        selected.textContent = e.target.textContent;
     }
 
-    if(e.target.textContent=="Design"){
+    if (e.target.textContent == "Design") {
         for (let i = 0; i < card.length; i++) {
             card[i].classList.add('bw-filter');
-          }
-        
+        }
+
     }
 
-    if(e.target.textContent=="Website"){
+    if (e.target.textContent == "Website") {
         for (let i = 0; i < card.length; i++) {
             card[i].classList.remove('bw-filter');
-          }
+        }
     }
 });
 
 //Nav blur
-const sliding_page=document.querySelector('.sliding-page');
-const navbar=document.querySelector('.navbar');
-document.addEventListener("scroll",function(e){
-    
+const sliding_page = document.querySelector('.sliding-page');
+const navbar = document.querySelector('.navbar');
+document.addEventListener("scroll", function (e) {
+
     let rect = sliding_page.getBoundingClientRect();
     let x = rect.top;
-    console.log(x);
-    if(x<=0){  
+    if (x <= 0) {
         navbar.classList.add('blur');
     }
 
-    else{
+    else {
         navbar.classList.remove('blur');
     }
 
@@ -68,23 +67,23 @@ document.addEventListener("scroll",function(e){
 
 //hamberger-menu
 
-const menu_icon=document.querySelector('.mobile-icon');
-const menu_btn=document.querySelector('.menu');
-const cut_btn=document.querySelector('.cut');
-const left_side_nav=document.querySelector('.left-side-nav');
-const body=document.querySelector('body');
+const menu_icon = document.querySelector('.mobile-icon');
+const menu_btn = document.querySelector('.menu');
+const cut_btn = document.querySelector('.cut');
+const left_side_nav = document.querySelector('.left-side-nav');
+const body = document.querySelector('body');
 
-menu_icon.addEventListener('click',function(e){
-    
+menu_icon.addEventListener('click', function (e) {
+
     left_side_nav.classList.toggle('visible');
     document.body.classList.toggle("stop-scrolling");
     left_side_nav.classList.toggle('blur');
 
     let rect = sliding_page.getBoundingClientRect();
     let x = rect.top;
-    if(x<=0){  
+    if (x <= 0) {
     }
-    else{
+    else {
         navbar.classList.toggle('blur');
     }
 
